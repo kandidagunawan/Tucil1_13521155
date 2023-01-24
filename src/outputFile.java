2cf276c41bbfa7f4a58b460fa12e378129420fc1
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2023-01-24 21:23:17 +0700

    add filepath for saving output file

--- src/outputFile.java
+++ src/outputFile.java
@@ -13,12 +13,12 @@ public class outputFile {
         try {
             System.out.println("Masukkan nama file: ");
             filename = scan.nextLine();
-            File solution = new File(filename);
+            File solution = new File("test\\" + filename);
             while (!solution.createNewFile()) {
                 System.out.println("Nama file yang kamu inginkan sudah ada!");
                 System.out.println("Coba masukkan nama file yang lain: ");
                 filename = scan.nextLine();
-                solution = new File(filename);
+                solution = new File("test/" + filename);
             }
             System.out.println("File created: " + solution.getName());
 
@@ -30,7 +30,7 @@ public class outputFile {
     public static void solutionFile(Set<String> solutionFormula, Integer numOfSol, ArrayList<Integer> cards) {
         try {
             createFile();
-            FileWriter writer = new FileWriter(filename);
+            FileWriter writer = new FileWriter("test/" + filename);
             writer.write("SOLUSI DARI 24 GAME SOLVER \n");
             writer.write(
                     "Masukan : " + cards.get(0) + " " + cards.get(1) + " " + cards.get(2) + " " + cards.get(3) + "\n");
